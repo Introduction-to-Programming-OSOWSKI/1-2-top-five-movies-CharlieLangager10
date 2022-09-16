9b7ef1b6ce6dce34ec42be9062040a740c844735
CharlieLangager10 <819272@mystma.org>
2022-09-16 09:27:12 -0500

    4

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from re import A
 
 
 def topFiveMovies(a, b, c, d, e):
-    return a + b + c + d + e 
+    return a +" " +b +" " +c +" "+ d+ " "+ e 
 print(topFiveMovies("top gun", "spiderman" , "outerbanks" , "stranger things", "thour"))
