4a8c64d7297722bd4c660e94238957a539db943a
CharlieLangager10 <819272@mystma.org>
2022-09-16 09:17:28 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#WRITE YOUR CODE IN THIS FILE
+from re import A
+
+
+def topFiveMovies():
+    return a + b + c + d + e 
+topFiveMovies("top gun", "spiderman" , "outerbanks" , "stranger things", "thour")
